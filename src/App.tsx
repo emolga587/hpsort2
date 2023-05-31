4b172e5db68f11f4c78de1dcc920edc362a157e9
kaisei-s <suzuki.kaisei@kyodonews.jp>
2023-05-31 15:51:25 +0900

    反映漏れ

--- src/App.tsx
+++ src/App.tsx
@@ -62,7 +62,7 @@ class Home extends React.Component<Props, State> {
           */
         }
         <Grid container item xs={12} justifyContent="center" spacing={0}>
-          <p>(最終更新:23/4/30 新研修生の追加)</p>
+          <p>(最終更新:23/5/31 メンバー情報の更新)</p>
         </Grid>
         <Grid container item xs={12} justifyContent="center" spacing={0}>
           <p><a href="./" onClick={helpAlert}>🔰ハロプロソートとは？🔰</a><br></br><br></br></p>
